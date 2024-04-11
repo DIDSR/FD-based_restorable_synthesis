3375e38d800dc55a6be1b473d54b108a616a36ac
Shuyue Guan <frankshuyueguan@gwu.edu>
2024-04-11 12:22:40 -0400

    Update FD_change.m

--- FD_change.m
+++ FD_change.m
@@ -1,6 +1,14 @@
 function out = FD_change(Fcode,l,h,sigma)
-Len = size(Fcode,1);% Len must be an Odd#
-n=(Len-1)/2;
+
+Len = size(Fcode,1);
+
+% deal with even and odd nums
+if mod(Len,2)
+    n=(Len-1)/2;
+else
+    n=Len/2;
+end
+
 % idx: 1,2,3,…,n,n+1, Len-n+1, Len-n+2,…,Len
 % idx: 1,2,3,…,n-1,n,n+1 (DC),n+2,n+3,…,2n-1,2n,2n+1
 % shifted idx, n is at DC
@@ -12,19 +20,19 @@ n=(Len-1)/2;
     % if l=0: include n(DC)
     % if l>0: keep no change on n(DC)
     
-%     l=2; h=10;
+    % l=2; h=10;
     
     s_i = [(n-h):(n-l),(n+l):(n+h)];
     idx = s_i + 1;
 
-%     sigma= 1;
+    % sigma= 1;
+    
     
     for i=1:length(idx)
         Fcode(idx(i),1)=Fcode(idx(i),1)*(1+normrnd(0,sigma));
         Fcode(idx(i),2)=Fcode(idx(i),2)*(1+normrnd(0,sigma));
     end
-
-
+    
 out = Fcode;
 
 end
