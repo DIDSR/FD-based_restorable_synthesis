2c869c3bce1c8c5375c6c2993366636d9d3ac242
Shuyue Guan <frankshuyueguan@gwu.edu>
2024-04-11 12:22:21 -0400

    Update Fdesc2bd.m

--- Fdesc2bd.m
+++ Fdesc2bd.m
@@ -1,25 +1,10 @@
 function o = Fdesc2bd(Fdesc,SIZE)
-% Fdesc = round(Fdesc*10e+5);
-len = Fdesc(end,1);
-if mod(len,2) % odd
-    len = len-1;
-end
-rc = fix(len/2)+1;
 
-Fdesc(end,:)=[];
-code_len = length(Fdesc);
-
-bg = rc-((code_len-1)/2);
-ed = rc+((code_len-1)/2);
-
+len = size(Fdesc);
 ii=Fdesc(:,1);jj=Fdesc(:,2);
 cmplx=ii+1i*jj;
 
-border_cmplx=zeros(1,len);
-border_cmplx(bg:ed) = cmplx;
-
-border_ifft =ifft(ifftshift(border_cmplx));
-
+border_ifft =ifft(ifftshift(cmplx));
 border_restored = zeros(SIZE);
 xx=round(real(border_ifft));yy=round(imag(border_ifft));
 try
