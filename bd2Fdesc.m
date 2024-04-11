614896ab7ccb745fe14518d7fe27681f5ffc376c
Shuyue Guan <frankshuyueguan@gwu.edu>
2024-04-11 12:22:01 -0400

    Update bd2Fdesc.m

--- bd2Fdesc.m
+++ bd2Fdesc.m
@@ -1,10 +1,8 @@
-function o = bd2Fdesc(img,ncoef)
+function o = bd2Fdesc(img)
 
 img(img~=0)=1;
 img = bwperim(img);
 image_edged = double(img);
-
-border_fft=zeros(size(image_edged));
 
 % find border
 f=find(image_edged);
@@ -15,36 +13,8 @@ border_cmplx=ii+1i*jj;
 
 border_fft =fftshift(fft(border_cmplx));
 
-if mod(lenf,2) % odd
-    lenf = lenf-1;
-end
-rc = fix(lenf/2)+1;
+o = zeros(lenf,2);
+o(:,1)=real(border_fft);
+o(:,2)=imag(border_fft);
 
-p1=[ (rc+1):(rc+1+ncoef-1)];
-p2=[ (rc-1):-1:(rc-1-ncoef+1)];
-
-o = zeros(2*ncoef+1,2);
-
-n = 1;
-
-
-for ind =(ncoef):-1:1
-    o(n,1)=real(border_fft(p2(ind)));
-    o(n,2)=imag(border_fft(p2(ind)));
-    n=n+1;
-end
-
-%add dc
-o(n,1)=real(border_fft(rc));
-o(n,2)=imag(border_fft(rc));
-n=n+1;
-
-for ind = 1:(ncoef)
-    o(n,1)=real(border_fft(p1(ind)));
-    o(n,2)=imag(border_fft(p1(ind)));
-    n=n+1;
-end
-o(n,1)=length(f);
-o(n,2)=length(f);
-% o=o/10e+5;
 end
